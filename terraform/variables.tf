293fc9844a201e15dac5e665b5fa968645b524ff
Manohar083063 <76534870+Manohar083063@users.noreply.github.com>
2025-09-03 12:40:43 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,47 +1,41 @@
 variable "app_name" {
-description = "Application/stack name"
-type = string
-default = "hello-fargate"
+  description = "Application/stack name"
+  type        = string
+  default     = "hello-fargate"
 }
-
 
 variable "aws_region" {
-description = "AWS region"
-type = string
-default = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
-
 
 variable "container_port" {
-description = "Container listen port"
-type = number
-default = 8080
+  description = "Container listen port"
+  type        = number
+  default     = 8080
 }
-
 
 variable "desired_count" {
   description = "Number of tasks"
-  type = number
-  default = 1
+  type        = number
+  default     = 1
 }
-
 
 variable "cpu" {
   description = "Fargate task CPU units"
-  type = number
-  default = 256
+  type        = number
+  default     = 256
 }
-
 
 variable "memory" {
   description = "Fargate task memory (MiB)"
-  type = number
-  default = 512
+  type        = number
+  default     = 512
 }
-
 
 variable "health_check_path" {
   description = "ALB health check path"
-  type = string
-  default = "/health"
+  type        = string
+  default     = "/health"
 }
